1ee5a9b8724bb6777e7eee12685a8e397377f1a6
laishengkai20050204 <48379994+laishengkai20050204@users.noreply.github.com>
2026-07-21 08:12:32 +0800

    Enable lite configuration properties

--- apps/javelin-lite/src/main/java/com/example/javelinlite/JavelinLiteApplication.java
+++ apps/javelin-lite/src/main/java/com/example/javelinlite/JavelinLiteApplication.java
@@ -2,8 +2,10 @@ package com.example.javelinlite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class JavelinLiteApplication {
 
     public static void main(String[] args) {
